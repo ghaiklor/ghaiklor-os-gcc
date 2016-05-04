c07455736b25adbe18f719bb9aa9cb0def2958a3
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-04 19:03:12 +0300

    Prints hello in boot sector

--- boot_sector_simple.asm
+++ boot_sector_simple.asm
@@ -1,5 +1,19 @@
-loop:
-	jmp loop
+mov ah, 0x0E
 
-times 510-($-$$) db 0
+mov al, 'H'
+int 0x10
+
+mov al, 'e'
+int 0x10
+
+mov al, 'l'
+int 0x10
+int 0x10
+
+mov al, 'o'
+int 0x10
+
+jmp $
+
+times 510 - ($-$$) db 0
 dw 0xAA55

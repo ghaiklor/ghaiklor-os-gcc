eb3005ebc6f798ddf7a3fb37edf9c3792842fbbb
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-05 16:42:52 +0300

    Add comments to print new line routine

--- boot/print_nl.asm
+++ boot/print_nl.asm
@@ -1,11 +1,15 @@
 print_nl:
+	;; store all register values
 	pusha
 
+	;; prepare for BIOS interrupt
+	;; write new line and carriage return
 	mov ah, 0x0E
 	mov al, 0x0A
 	int 0x10
 	mov al, 0x0D
 	int 0x10
 
+	;; restore all registers
 	popa
 	ret

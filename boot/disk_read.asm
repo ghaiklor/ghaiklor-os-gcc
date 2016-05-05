039f9254faadefdd9f56eb4dfac2a23c68e977bd
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-05 13:51:42 +0300

    Implement disk_read

---
+++ boot/disk_read.asm
@@ -0,0 +1,38 @@
+disk_read:
+	;; store all register values
+	pusha
+	push dx
+
+	;; prepare data for reading the disk
+	;; so BIOS interrupt know what to read
+	;; al = number of sectors to read (1 - 128)
+	;; ch = track/cylinder number
+	;; dh = head number
+	;; cl = sector number
+	mov ah, 0x02
+	mov al, dh
+	mov ch, 0x00
+	mov dh, 0x00
+	mov cl, 0x02
+	int 0x13
+
+	;; in case of read error
+	;; show the message about it
+	jc disk_read_error
+
+	;; check if we read expected count of sectors
+	;; if not, show the message with error
+	pop dx
+	cmp dh, al
+	jne disk_read_error
+
+	;; restore register values and ret
+	popa
+	ret
+
+disk_read_error:
+	mov bx, DISK_READ_ERROR_MSG
+	call print_string
+	jmp $
+
+DISK_READ_ERROR_MSG: db "Disk read error!", 0
--- boot/main.asm
+++ boot/main.asm
@@ -1,17 +1,34 @@
 [org 0x7C00]
 
-mov bx, BOOT_FOUND_MESSAGE
+mov bx, BOOT_SECTOR_FOUND_MSG
 call print_string
 
-mov dx, 0x1A2B
+mov [BOOT_DRIVE], dl
+mov bp, 0x8000
+mov sp, bp
+
+mov bx, 0x9000
+mov dh, 5
+mov dl, [BOOT_DRIVE]
+call disk_read
+
+mov dx, [0x9000]
+call print_hex
+
+mov dx, [0x9000 + 512]
 call print_hex
 
 jmp $
 
 %include "boot/print_string.asm"
 %include "boot/print_hex.asm"
+%include "boot/disk_read.asm"
 
-BOOT_FOUND_MESSAGE: db "Found boot sector, loading...", 0
+BOOT_SECTOR_FOUND_MSG: db "Found boot sector, loading...", 0
+BOOT_DRIVE: db 0
 
 times 510 - ($-$$) db 0
 dw 0xAA55
+
+times 256 dw 0xdada
+times 256 dw 0xface

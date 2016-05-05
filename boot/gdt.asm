bcbea17805db09217c8aedb674a29842445f9475
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-05 15:58:28 +0300

    Declare GDT

---
+++ boot/gdt.asm
@@ -0,0 +1,28 @@
+gdt_start:
+gdt_null:
+	dd 0x0
+	dd 0x0
+
+gdt_code:
+	dw 0xFFFF
+	dw 0x0
+	db 0x0
+	db 10011010b
+	db 11001111b
+	db 0x0
+
+gdt_data:
+	dw 0xFFFF
+	dw 0x0
+	db 0x0
+	db 10010010b
+	db 11001111b
+	db 0x0
+
+gdt_end:
+gdt_descriptor:
+	dw gdt_end - gdt_start - 1
+	dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start

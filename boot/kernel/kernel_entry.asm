ca54f2260ceba93c91389c06c400a743e0019c46
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-06 13:47:40 +0300

    Add comments to kernel_entry.asm

--- boot/kernel/kernel_entry.asm
+++ boot/kernel/kernel_entry.asm
@@ -1,4 +1,6 @@
 ;; Routine that jump direct into main() in kernel
+;; This routine is linked with kernel main.c file
+;; at kernel folder, so we can call main() explicitly
 
 [bits 32]
 [extern main]

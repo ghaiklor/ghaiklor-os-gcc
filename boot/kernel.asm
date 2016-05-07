c45b42d4832f4fa776d282e7b3cd23a66a3b6bc3
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-07 11:14:55 +0300

    docs: add comments to kernel.asm

--- boot/kernel.asm
+++ boot/kernel.asm
@@ -1,3 +1,8 @@
+;; Kernel entry point
+;; Call main() in kernel explicitly
+;; so we can be sure we are not messing with addresses
+;; It compiles to object file and links with kernel
+
 [bits 32]
 [extern main]
 call main

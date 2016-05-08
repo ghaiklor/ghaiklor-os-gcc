6770b6483ba0816ebb8031b8b98fb4185c72b04c
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-08 12:29:41 +0300

    docs: add more comments to kernel_entry

--- boot/kernel_entry.asm
+++ boot/kernel_entry.asm
@@ -1,7 +1,12 @@
-;; Kernel entry point
-;; Call main() in kernel explicitly
-;; so we can be sure we are not messing with addresses
-;; It compiles to object file and links with kernel
+;; We can't be sure we are executing the main() method in kernel
+;; boot sector and kernel are combined together
+;; boot calls the instruction at 0x7C00 + 0x1000
+;; no matter what in there
+;; A trick here is to write a simple sub-routine
+;; that is attached to the start of the kernel code
+;; and calls the entry function of the kernel
+;; When object files will be linked together
+;; this call will be translated to the address of the main()
 
 [bits 32]
 [extern main]

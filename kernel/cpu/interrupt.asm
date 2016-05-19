a309a8bf3da7e7ad75dd41ab586cd38f48868564
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-19 11:19:38 +0300

    isr: got working interrupt gates

---
+++ kernel/cpu/interrupt.asm
@@ -0,0 +1,242 @@
+[extern isr_handler]
+
+isr_common_stub:
+  pusha
+  mov ax, ds
+  push eax
+  mov ax, 0x10
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+
+  call isr_handler
+
+  pop eax
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  popa
+  add esp, 8
+  sti
+  iret
+
+global isr0
+global isr1
+global isr2
+global isr3
+global isr4
+global isr5
+global isr6
+global isr7
+global isr8
+global isr9
+global isr10
+global isr11
+global isr12
+global isr13
+global isr14
+global isr15
+global isr16
+global isr17
+global isr18
+global isr19
+global isr20
+global isr21
+global isr22
+global isr23
+global isr24
+global isr25
+global isr26
+global isr27
+global isr28
+global isr29
+global isr30
+global isr31
+
+isr0:
+  cli
+  push byte 0
+  push byte 0
+  jmp isr_common_stub
+
+isr1:
+  cli
+  push byte 0
+  push byte 1
+  jmp isr_common_stub
+
+isr2:
+  cli
+  push byte 0
+  push byte 2
+  jmp isr_common_stub
+
+isr3:
+  cli
+  push byte 0
+  push byte 3
+  jmp isr_common_stub
+
+isr4:
+  cli
+  push byte 0
+  push byte 4
+  jmp isr_common_stub
+
+isr5:
+  cli
+  push byte 0
+  push byte 5
+  jmp isr_common_stub
+
+isr6:
+  cli
+  push byte 0
+  push byte 6
+  jmp isr_common_stub
+
+isr7:
+  cli
+  push byte 0
+  push byte 7
+  jmp isr_common_stub
+
+isr8:
+  cli
+  push byte 8
+  jmp isr_common_stub
+
+isr9:
+  cli
+  push byte 0
+  push byte 9
+  jmp isr_common_stub
+
+isr10:
+  cli
+  push byte 10
+  jmp isr_common_stub
+
+isr11:
+  cli
+  push byte 11
+  jmp isr_common_stub
+
+isr12:
+  cli
+  push byte 12
+  jmp isr_common_stub
+
+isr13:
+  cli
+  push byte 13
+  jmp isr_common_stub
+
+isr14:
+  cli
+  push byte 14
+  jmp isr_common_stub
+
+isr15:
+  cli
+  push byte 0
+  push byte 15
+  jmp isr_common_stub
+
+isr16:
+  cli
+  push byte 0
+  push byte 16
+  jmp isr_common_stub
+
+isr17:
+  cli
+  push byte 0
+  push byte 17
+  jmp isr_common_stub
+
+isr18:
+  cli
+  push byte 0
+  push byte 18
+  jmp isr_common_stub
+
+isr19:
+  cli
+  push byte 0
+  push byte 19
+  jmp isr_common_stub
+
+isr20:
+  cli
+  push byte 0
+  push byte 20
+  jmp isr_common_stub
+
+isr21:
+  cli
+  push byte 0
+  push byte 21
+  jmp isr_common_stub
+
+isr22:
+  cli
+  push byte 0
+  push byte 22
+  jmp isr_common_stub
+
+isr23:
+  cli
+  push byte 0
+  push byte 23
+  jmp isr_common_stub
+
+isr24:
+  cli
+  push byte 0
+  push byte 24
+  jmp isr_common_stub
+
+isr25:
+  cli
+  push byte 0
+  push byte 25
+  jmp isr_common_stub
+
+isr26:
+  cli
+  push byte 0
+  push byte 26
+  jmp isr_common_stub
+
+isr27:
+  cli
+  push byte 0
+  push byte 27
+  jmp isr_common_stub
+
+isr28:
+  cli
+  push byte 0
+  push byte 28
+  jmp isr_common_stub
+
+isr29:
+  cli
+  push byte 0
+  push byte 29
+  jmp isr_common_stub
+
+isr30:
+  cli
+  push byte 0
+  push byte 30
+  jmp isr_common_stub
+
+isr31:
+  cli
+  push byte 0
+  push byte 31
+  jmp isr_common_stub
---
+++ kernel/cpu/isr.c
@@ -0,0 +1,90 @@
+#include "isr.h"
+#include "idt.h"
+#include "../../drivers/screen.h"
+#include "../util.h"
+
+char *exception_messages[] = {
+  "Division By Zero",
+  "Debug",
+  "Non Maskable Interrupt",
+  "Breakpoint",
+  "Into Detected Overflow",
+  "Out of Bounds",
+  "Invalid Opcode",
+  "No Coprocessor",
+
+  "Double Fault",
+  "Coprocessor Segment Overrun",
+  "Bad TSS",
+  "Segment Not Present",
+  "Stack Fault",
+  "General Protection Fault",
+  "Page Fault",
+  "Unknown Interrupt",
+
+  "Coprocessor Fault",
+  "Alignment Check",
+  "Machine Check",
+  "Reserved",
+  "Reserved",
+  "Reserved",
+  "Reserved",
+  "Reserved",
+
+  "Reserved",
+  "Reserved",
+  "Reserved",
+  "Reserved",
+  "Reserved",
+  "Reserved",
+  "Reserved",
+  "Reserved"
+};
+
+void isr_install() {
+  set_idt_gate(0, (uint32_t)isr0);
+  set_idt_gate(1, (uint32_t)isr1);
+  set_idt_gate(2, (uint32_t)isr2);
+  set_idt_gate(3, (uint32_t)isr3);
+  set_idt_gate(4, (uint32_t)isr4);
+  set_idt_gate(5, (uint32_t)isr5);
+  set_idt_gate(6, (uint32_t)isr6);
+  set_idt_gate(7, (uint32_t)isr7);
+  set_idt_gate(8, (uint32_t)isr8);
+  set_idt_gate(9, (uint32_t)isr9);
+  set_idt_gate(10, (uint32_t)isr10);
+  set_idt_gate(11, (uint32_t)isr11);
+  set_idt_gate(12, (uint32_t)isr12);
+  set_idt_gate(13, (uint32_t)isr13);
+  set_idt_gate(14, (uint32_t)isr14);
+  set_idt_gate(15, (uint32_t)isr15);
+  set_idt_gate(16, (uint32_t)isr16);
+  set_idt_gate(17, (uint32_t)isr17);
+  set_idt_gate(18, (uint32_t)isr18);
+  set_idt_gate(19, (uint32_t)isr19);
+  set_idt_gate(20, (uint32_t)isr20);
+  set_idt_gate(21, (uint32_t)isr21);
+  set_idt_gate(22, (uint32_t)isr22);
+  set_idt_gate(23, (uint32_t)isr23);
+  set_idt_gate(24, (uint32_t)isr24);
+  set_idt_gate(25, (uint32_t)isr25);
+  set_idt_gate(26, (uint32_t)isr26);
+  set_idt_gate(27, (uint32_t)isr27);
+  set_idt_gate(28, (uint32_t)isr28);
+  set_idt_gate(29, (uint32_t)isr29);
+  set_idt_gate(30, (uint32_t)isr30);
+  set_idt_gate(31, (uint32_t)isr31);
+
+  set_idt();
+}
+
+void isr_handler(registers_t r) {
+  char s[3];
+
+  print("Received interrupt: ");
+  itoa(r.int_no, s);
+  print(s);
+  print("\n");
+  print(exception_messages[r.int_no]);
+  print("\n");
+}

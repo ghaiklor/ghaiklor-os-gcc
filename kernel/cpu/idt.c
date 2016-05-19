39e814e3703a8f6b5f487610720a29fb2a007eb7
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-19 14:33:34 +0300

    docs: add few comment blocks into IDT implementation

--- kernel/cpu/idt.c
+++ kernel/cpu/idt.c
@@ -1,6 +1,11 @@
+// Implementation for Interrupt Descriptor Table
+// http://wiki.osdev.org/Interrupt_Descriptor_Table
+
 #include "idt.h"
 #include "../util.h"
 
+// Register gate in Interrupt Descriptor Table
+// It stores in declared in idt.h variable called idt
 void set_idt_gate(int n, uint32_t handler) {
   idt[n].low_offset = LOW_16(handler);
   idt[n].sel = KERNEL_CS;
@@ -9,6 +14,10 @@ void set_idt_gate(int n, uint32_t handler) {
   idt[n].high_offset = HIGH_16(handler);
 }
 
+// Set base address where IDT starts in idt_reg.base
+// Set the length of the IDT in bytes into idt_reg.limit
+// Call lidtl (Load Interrupt Descriptor Table) instruction
+// And set address of our IDT header
 void set_idt() {
   idt_reg.base = (uint32_t)&idt;
   idt_reg.limit = IDT_ENTRIES * sizeof(idt_gate_t) - 1;
--- kernel/cpu/idt.h
+++ kernel/cpu/idt.h
@@ -1,8 +1,18 @@
 #include "../../include/stdint.h"
 
+// Address of kernel code segment
 #define KERNEL_CS 0x08
+
+// Entries count in Interrupt Descriptor Table
 #define IDT_ENTRIES 256
 
+// Structure for storing Gate entry
+// http://wiki.osdev.org/Interrupt_Descriptor_Table#Location_and_Size
+// 0..15 bits are Lo offset bits
+// 16..31 bits is a code segment selector in GDT or LDT
+// 32..39 bits are unused, set to 0
+// 40..47 bits are type and attributes bits
+// 48..63 bits are Hi offset bits
 typedef struct {
   uint16_t low_offset;
   uint16_t sel;
@@ -11,13 +21,21 @@ typedef struct {
   uint16_t high_offset;
 } __attribute__((packed)) idt_gate_t;
 
+// Structure for storing IDT location
+// http://wiki.osdev.org/Interrupt_Descriptor_Table#Location_and_Size
+// 0..15 bits defines the length of IDT
+// 16..47 bits defines address where IDT starts
 typedef struct {
   uint16_t limit;
   uint32_t base;
 } __attribute__((packed)) idt_register_t;
 
+// Declare array of IDT entries
 idt_gate_t idt[IDT_ENTRIES];
+
+// Declare structure with info about IDT location
 idt_register_t idt_reg;
 
+// Public API
 void set_idt_gate(int n, uint32_t handler);
 void set_idt();
